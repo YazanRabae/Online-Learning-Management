f279406499672e0874954dbffbf7b27b7a235c97
3bd002 <abdelrahman1202@hotmail.com>
2024-08-14 12:40:50 +0300

    init Roles

--- Online Learning Management/Controllers/AdminController.cs
+++ Online Learning Management/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Online_Learning_Management.Controllers
 {
+    //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
